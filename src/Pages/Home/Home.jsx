7b48d47c281aebe8b4784e8f0306e43562c33601
Belachew <belaye1952@gmail.com>
2025-02-18 21:54:05 -0500

    this is the last and finale updated. Footer jsx and Footer module is created.

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Banner from "../../components/Banner/Banner"
+import Footer from "../../components/Footer/Footer"
+import Header from "../../components/Header/Header"
+import RowList from "../../components/Row/RowList"
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <Banner/>
+    <RowList/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { FaFacebook } from "react-icons/fa6";
+import {IoLogoInstagram} from 'react-icons/io5';
+import {BsYoutube} from "react-icons/bs";
+import styles from "./Footer.module.css";
+function Footer() {
+  return (
+    <>
+      <div className={styles.outer_container}>
+        <div>
+            <ul>
+<li>
+<FaFacebook size={22}/>
+<IoLogoInstagram size={22}/>
+<BsYoutube size={22}/>
+</li>
+<li>
+    <a href = "">FAQ</a>
+</li>
+<li>
+    <a href = "">Media Center</a>
+</li>
+<li>
+    <a href = "">Ways to Watch</a>
+</li>
+<li>
+    <a href = "">Cookie Perferences</a>
+</li>
+            </ul>
+        </div>
+    <div>
+    <li>
+    <a href = "">Jobs</a>
+    </li>
+    <li>
+        <a href="">Praivasy</a>
+    </li>
+    <li className={styles.only_mobile}/>
+    <li>
+    <a href = "">Contact Us</a>
+    </li>
+    </div>
+    <div>
+            <ul className={styles.footer_right_col}>
+<li>
+    <a href="">only on Netflix</a>
+</li>
+<li>
+    <a href="">Speed Test</a>
+</li>
+<li>
+<a href="">Speed Test</a>
+</li>
+</ul>
+<div>
+    <ul>
+        <li>
+            <a href="">Help Center</a>
+        </li>
+        <li>
+            <a href="">Investor Relations</a>
+        </li>
+        <li>
+            <a href="">Terms of Use</a>
+        </li>
+        <li>
+            <a href="">Corporate Information</a>
+        </li>
+        <li>
+            <a href="">Legal  Notices</a>
+        </li>
+    </ul>
+</div>
+</div>
+        </div>
+</>
+ 
+  )
+}
+
+export default Footer
